433b54d323c203527443a73a1c4a65a599040b7c
lfarcaro <48957854+lfarcaro@users.noreply.github.com>
2019-05-07 14:20:16 -0300

    Script de geração automática.

--- admin/composer_bake.bat
+++ admin/composer_bake.bat
@@ -1,3 +1,11 @@
 SET PATH=%PATH%;C:\xampp\php
 call bin\cake bake all categorias --force
+call bin\cake bake all administradores --force
+call bin\cake bake all designers --force
 call bin\cake bake all paginas --force
+call bin\cake bake all artigos --force
+call bin\cake bake all projetos --force
+call bin\cake bake all desfiles --force
+call bin\cake bake model artigo_fotografias --force
+call bin\cake bake model projeto_fotografias --force
+call bin\cake bake model desfile_fotografias --force
